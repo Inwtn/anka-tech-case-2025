ce8142ffd93c7b5079ee25cadfa95de926a84168
Inwtn <lutevl12@gmail.com>
2025-06-06 01:12:12 -0300

    comecando front end

---
+++ frontend/src/app/ativos/page.tsx
@@ -0,0 +1,8 @@
+export default function AtivosPage() {
+  return (
+    <main className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Ativos</h1>
+      <p>Essa é a página de ativos!</p>
+    </main>
+  );
+}
